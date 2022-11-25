829c264fbb125acdeb68fed46308f086cf690b1d
Aryan <aryan16072001236809@gmail.com>
2022-11-25 23:46:57 +0530

    Fetching new cards with Ajax

--- src/Deck.js
+++ src/Deck.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-const API_URL="https://deckofcardsapi.com/api/deck/new/shuffle";
+const API_BASE_URL="https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
     constructor(props){
         super(props);
-        this.state={deck:null};
+        this.state={deck:null,drawn:[]};
         this.getCard=this.getCard.bind(this);
     }
 
     async componentDidMount()
     {
-        let deck= await axios.get(API_URL);
+        let deck= await axios.get(`${API_BASE_URL}/new/shuffle`);
         this.setState({deck:deck.data})
     }
-    getCard(){
-
-
+    async getCard(){
+        let deck_id= this.state.deck.deck_id;
+        try{
+          let cardUrl=`${API_BASE_URL}/${deck_id}/draw/`;
+          let cardRes=await axios.get(cardUrl);
+          if(!cardRes.data.success){
+            throw new Error("No card remaining");
+          }
+        
+      
+      let card=cardRes.data.cards[0];
+      this.setState(st=>({
+        drawn:[
+          ...st.drawn,
+        {
+          id:card.code,
+            image:card.image,
+          name:`${card.value} ${card.suit} `
+        }
+        ]
+      }));
+        // "https://deckofcardsapi.com/api/deck/${deck_id}/draw/"
+  
+    }catch(err){
+alert(err);
+    }
     }
   render() {
     return (
